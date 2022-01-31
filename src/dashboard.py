cafa97f50254b2edeb510bc502a9ffdb7a83069d
Donald Cheng Hong Zou <donaldzou@live.hk>
2022-01-31 16:09:50 -0500

    Changed version number

--- src/dashboard.py
+++ src/dashboard.py
@@ -31,7 +31,7 @@ from util import regex_match, check_DNS, check_Allowed_IPs, check_remote_endpoin
     check_IP_with_range, clean_IP_with_range
 
 # Dashboard Version
-DASHBOARD_VERSION = 'v3.0.3'
+DASHBOARD_VERSION = 'v3.0.5'
 # WireGuard's configuration path
 WG_CONF_PATH = None
 # Dashboard Config Name
