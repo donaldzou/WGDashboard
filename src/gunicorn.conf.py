9dd57508a5b840a29f01d97b1073bf13dccbe95f
Abel Alfonso Fírvida Donéstevez <aafirvida@gmail.com>
2022-02-07 19:10:23 -0500

    Listen only in local interface

--- src/gunicorn.conf.py
+++ src/gunicorn.conf.py
@@ -1,7 +1,8 @@
 import multiprocessing
 import dashboard
 
-app_host, app_port = dashboard.get_host_bind()
+app_host = "localhost"
+app_port = 8000
 
 worker_class = 'gthread'
 workers = multiprocessing.cpu_count() * 2 + 1
