62beae94864f5245a25c4d63a75c4f813faf0ba9
theonlynexus <massimo.fierro@gmail.com>
2022-07-21 06:05:05 +0000

    Fixed default WG config path

--- src/default_config.py
+++ src/default_config.py
@@ -6,7 +6,7 @@ dash_config = {
         "password": "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918",
     },
     "Server": {
-        "wg_conf_path": "/config",
+        "wg_conf_path": "/etc/wireguard",
         "auth_req": True,
         "dashboard_refresh_interval": 60000,
         "dashboard_sort": "status",
