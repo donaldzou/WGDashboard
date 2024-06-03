ceb045a61140337c473cc9c6d32298396b554b64
brandon-layton <bjlayton@protonmail.com>
2024-06-02 18:57:31 -0600

    added option to run newer dashboard in docker through env variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,18 @@
 #   echo "Removed unneeded conf file"
 # fi
 
+# if they choose to run old
+if [[ ${DASHBOARD_VERSON^^} == "NEW" ]] ; then
+  echo "Using newer dashboard"
+  cp dashboard.py dashboard_old.py
+  cp dashboard_new.py dashboard.py
+else
+  echo "Defaulting to old dashboard"
+  if ! $(cp dashboard_old.py dashboard.py 2>/dev/null) ; then
+    echo "No dashboard_old.py file, assuming dashboard.py version is old"
+  fi
+fi
+
 # wg-quick up wg0
 chmod u+x /opt/wgdashboard/wgd.sh
 if [ ! -f "/opt/wgdashboard/wg-dashboard.ini" ]; then
