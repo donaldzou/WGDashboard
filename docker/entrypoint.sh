0938f5ab7113854c391d62c7e2fe646a5c7d028b
Daan <dselen@systemec.nl>
2024-06-04 22:49:17 +0200

    Container deployed with working VPN server built-in. Missing is persistency among recreations, looking into that...

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,15 +1,25 @@
 echo "Starting the WireGuard Dashboard."
 
-outgoing=$(ip -o -4 route show to default | awk '{print $NF}')
-echo $outgoing
-
+# Starting the WireGuard Dashboard Web-UI.
 . ${WGDASH}/venv/bin/activate
-cd /opt/wireguardashboard/app/src
+cd ${WGDASH}/app/src
 bash ./wgd.sh start
 
 if [ "$tz" != "Europe/Amsterdam" ]; then
   echo "Changing timezone..."
   ln -sf /usr/share/zoneinfo/$tz /etc/localtime
+fi
+
+if [ "$global_dns" != "1.1.1.1" ]; then # Changing the DNS used for clients. Had to change it in 2 locations.
+  echo "Changing default dns..."
+  sed -i 's/^DNS = .*/DNS = ${global_dns}/' /etc/wireguard/wg0.conf
+  sed -i "s/^peer_global_dns = .*/peer_global_dns = $global_dns/" /opt/wireguardashboard/app/src/wg-dashboard.ini
+fi
+
+if [ "$public_ip" != "0.0.0.0" ]; then # Setting the public IP of the WireGuard Dashboard container host. If not defined, it will be tried using ifconfig.me.
+  sed -i "s/^remote_endpoint = .*/remote_endpoint = $public_ip/" /opt/wireguardashboard/app/src/wg-dashboard.ini
+else
+  sed -i "s/^remote_endpoint = .*/remote_endpoint = $(curl ifconfig.me)/" /opt/wireguardashboard/app/src/wg-dashboard.ini
 fi
 
 sleep 3s
