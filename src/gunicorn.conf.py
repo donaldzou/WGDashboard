461ae99dd8c89f672c41322131e625095eed97c1
Donald Zou <donaldzou@live.hk>
2024-08-02 21:48:42 -0400

    Testing again...

--- src/gunicorn.conf.py
+++ src/gunicorn.conf.py
@@ -1,6 +1,7 @@
 import multiprocessing
 import dashboard
 
+global sqldb, cursor, DashboardConfig, WireguardConfigurations, AllPeerJobs, JobLogger
 app_host, app_port = dashboard.gunicornConfig()
 
 worker_class = 'gthread'
