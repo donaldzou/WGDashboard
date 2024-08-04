b3bd6bb39ebbb117eafde2e027ce0d3319d66d70
Donald Zou <donaldzou@live.hk>
2024-08-04 19:20:31 -0400

    Update wgd.sh

--- src/wgd.sh
+++ src/wgd.sh
@@ -239,6 +239,7 @@ gunicorn_start () {
   _check_and_set_venv
   sudo "$venv_gunicorn" --access-logfile log/access.log \
   --log-level 'debug' --capture-output \
+  --config ./gunicorn.conf.py --print_config
   --error-logfile log/error.log --daemon 'dashboard:app'
   sleep 5
   checkPIDExist=0
