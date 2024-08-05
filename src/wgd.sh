8c8374a08c8c317596bb642c37f12c141c7149e1
Donald Zou <donaldzou@live.hk>
2024-08-04 20:16:26 -0400

    Update wgd.sh

--- src/wgd.sh
+++ src/wgd.sh
@@ -244,11 +244,11 @@ gunicorn_start () {
   do
   		if test -f './gunicorn.pid'; then
   			checkPIDExist=1
-  			printf "[WGDashboard] Checking if WGDashboard w/ Gunicorn started successfully"
+  			printf "[WGDashboard] Checking if WGDashboard w/ Gunicorn started successfully\n"
   		fi
   		sleep 2
   done
-  printf "[WGDashboard] WGDashboard w/ Gunicorn started successfully"
+  printf "[WGDashboard] WGDashboard w/ Gunicorn started successfully\n"
   printf "%s\n" "$dashes"
 }
 
