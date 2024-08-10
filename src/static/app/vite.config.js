fd3fc66bfc65091c2c62a1e9e3b13790235a917a
Donald Zou <donaldzou@live.hk>
2024-08-10 00:25:25 -0400

    Thinking of adding Electron.js

--- src/static/app/vite.config.js
+++ src/static/app/vite.config.js
@@ -5,27 +5,62 @@ import {proxy} from "./proxy.js";
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/static/app/dist",
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({mode}) => {
+  if (mode === 'production'){
+    return {
+      base: "/static/app/dist",
+      plugins: [
+        vue(),
+      ],
+      resolve: {
+        alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+      },
+      server:{
+        proxy: {
+          '/api': proxy
+        }
+      },
+      build: {
+        outDir: 'dist',
+        rollupOptions: {
+          output: {
+            entryFileNames: `assets/[name].js`,
+            chunkFileNames: `assets/[name].js`,
+            assetFileNames: `assets/[name].[ext]`
+          }
+        }
+      }
     }
-  },
-  server:{
-    proxy: {
-      '/api': proxy
-    }
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`
+  }
+
+  if (mode === 'electron'){
+    return {
+      emptyOutDir: false,
+      base: './',
+      plugins: [
+        vue(),
+      ],
+      resolve: {
+        alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+      },
+      server:{
+        proxy: {
+          '/api': proxy
+        }
+      },
+      build: {
+        outDir: 'electron',
+        rollupOptions: {
+          output: {
+            entryFileNames: `assets/[name].js`,
+            chunkFileNames: `assets/[name].js`,
+            assetFileNames: `assets/[name].[ext]`
+          }
+        }
       }
     }
   }
