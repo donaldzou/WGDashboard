a7bb5ac21b34d786abbbd35950b0bf1c28445e26
Donald Zou <donaldzou@live.hk>
2024-08-16 17:36:22 -0400

    Update wgd.sh

--- src/wgd.sh
+++ src/wgd.sh
@@ -257,7 +257,7 @@ install_wgd(){
     fi
     _check_and_set_venv
     printf "[WGDashboard] Upgrading Python Package Manage (PIP)\n"
-    { date; python3 -m pip install pip; printf "\n\n"; } >> ./log/install.txt
+    { date; python3 -m pip install --upgrade pip; printf "\n\n"; } >> ./log/install.txt
     printf "[WGDashboard] Installing latest Python dependencies\n"
     { date; python3 -m pip install -r requirements.txt ; printf "\n\n"; } >> ./log/install.txt
     printf "[WGDashboard] WGDashboard installed successfully!\n"
