00611ef9dca2def6ad882cc703621fa03dc548a7
Dselen <Dselen@nerthus.nl>
2024-08-20 09:58:25 -0500

    Progress so far.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,30 +1,48 @@
 #!/bin/bash
 echo "Starting the WireGuard Dashboard Docker container."
 
+# === CLEAN UP ===
 clean_up() {
+  echo "---------------------    CLEAN UP    -----------------------"
   # Cleaning out previous data such as the .pid file and starting the WireGuard Dashboard. Making sure to use the python venv.
   echo "Looking for remains of previous instances..."
-  if [ -f "/opt/wireguarddashboard/app/src/gunicorn.pid" ]; then
+  local pid_file="${WGDASH}/src/gunicorn.pid"
+  if [ -f $pid_file ]; then
     echo "Found old .pid file, removing."
-    rm /opt/wireguarddashboard/app/src/gunicorn.pid
+    rm $pid_file
   else
-    echo "No remains found, continuing."
+    echo "No pid remains found, continuing."
+  fi
+
+  local pycache="${WGDASH}/src/__pycache__"
+  if [ -d "$pycache" ]; then
+    local pycache_filecount=$(find "$pycache" -maxdepth 1 -type f | wc -l)
+    if [ "$pycache_filecount" -gt 0 ]; then
+      echo "Found old pycaches, removing."
+      rm -rf "$pycache"/*
+    else
+      echo "No pycaches found, continuing."
+    fi
+  else
+    echo "No pycaches found, continuing."
   fi
 }
 
+# === CORE SERVICES ===
 start_core() {
+  echo "---------------------  STARTING CORE -----------------------"
+
   # This first step is to ensure the wg0.conf file exists, and if not, then its copied over from the ephemeral container storage.
   if [ ! -f "/etc/wireguard/wg0.conf" ]; then
     cp "/wg0.conf" "/etc/wireguard/wg0.conf"
-    echo "WireGuard interface file copied over."
+    echo "Standard WG0 Configuration file not found, grabbing template."
   else
-    echo "WireGuard interface file looks to already be existing."
+    echo "Standard WG0 Configuration file found, using that."
   fi
   
   echo "Activating Python venv and executing the WireGuard Dashboard service."
-
-  . "${WGDASH}"/venv/bin/activate
-  cd "${WGDASH}"/app/src || return # If changing the directory fails (permission or presence error), then bash will exist this function, causing the WireGuard Dashboard to not be succesfully launched.
+  . "${WGDASH}"/src/venv/bin/activate
+  cd "${WGDASH}"/src || return # If changing the directory fails (permission or presence error), then bash will exist this function, causing the WireGuard Dashboard to not be succesfully launched.
   bash wgd.sh start
 
   # The following section takes care of the firewall rules regarding the 'isolated_peers' feature, which allows or drops packets destined from the wg0 to the wg0 interface.
@@ -45,17 +63,22 @@ start_core() {
 
   fi
 
-  # The following section takes care of 
-  if [ "${enable_wg0,,}" = "true" ]; then
-    echo "Preference for wg0 to be turned on found."
-
-    wg-quick up wg0
-  else
-    echo "Preference for wg0 to be turned off found."
-  fi
+  # The following section takes care of enabling wireguard interfaces on startup.
+  IFS=',' read -r -a enable_array <<< "${enable}"
+  for interface in "${enable_array[@]}"; do
+    echo "Preference for $interface to be turned on found."
+    if [ -f "/etc/wireguard/${interface}.conf" ]; then
+      echo "Found corresponding configuration file, activating..."
+      wg-quick up $interface
+    else
+      echo "No corresponding configuration file found for $interface doing nothing."
+    fi
+  done
 }
 
+# === SET ENV VARS ===
 set_envvars() {
+  echo "------------------------------------------------------------"
   echo "Setting relevant variables for operation."
 
   # If the timezone is different, for example in North-America or Asia.
@@ -67,11 +90,11 @@ set_envvars() {
   fi
 
   # Changing the DNS used for clients and the dashboard itself.
-  if [ "${global_dns}" != "$(grep "peer_global_dns = " /opt/wireguarddashboard/app/src/wg-dashboard.ini | awk '{print $NF}')" ]; then 
+  if [ "${global_dns}" != "$(grep "peer_global_dns = " /opt/wireguarddashboard/src/wg-dashboard.ini | awk '{print $NF}')" ]; then 
     echo "Changing default dns."
 
     #sed -i "s/^DNS = .*/DNS = ${global_dns}/" /etc/wireguard/wg0.conf # Uncomment if you want to have DNS on server-level.
-    sed -i "s/^peer_global_dns = .*/peer_global_dns = ${global_dns}/" /opt/wireguarddashboard/app/src/wg-dashboard.ini
+    sed -i "s/^peer_global_dns = .*/peer_global_dns = ${global_dns}/" /opt/wireguarddashboard/src/wg-dashboard.ini
   fi
 
   # Setting the public IP of the WireGuard Dashboard container host. If not defined, it will trying fetching it using a curl to ifconfig.me.
@@ -79,22 +102,24 @@ set_envvars() {
     default_ip=$(curl -s ifconfig.me)
     echo "Trying to fetch the Public-IP using ifconfig.me: ${default_ip}"
 
-    sed -i "s/^remote_endpoint = .*/remote_endpoint = ${default_ip}/" /opt/wireguarddashboard/app/src/wg-dashboard.ini
-  elif [ "${public_ip}" != "$(grep "remote_endpoint = " /opt/wireguarddashboard/app/src/wg-dashboard.ini | awk '{print $NF}')" ]; then
+    sed -i "s/^remote_endpoint = .*/remote_endpoint = ${default_ip}/" /opt/wireguarddashboard/src/wg-dashboard.ini
+  elif [ "${public_ip}" != "$(grep "remote_endpoint = " /opt/wireguarddashboard/src/wg-dashboard.ini | awk '{print $NF}')" ]; then
     echo "Setting the Public-IP using given variable: ${public_ip}"
 
-    sed -i "s/^remote_endpoint = .*/remote_endpoint = ${public_ip}/" /opt/wireguarddashboard/app/src/wg-dashboard.ini
+    sed -i "s/^remote_endpoint = .*/remote_endpoint = ${public_ip}/" /opt/wireguarddashboard/src/wg-dashboard.ini
   fi
 }
 
+# === CLEAN UP ===
 ensure_blocking() {
+  echo "------------------------------------------------------------"
   sleep 1s
   echo "Ensuring container continuation."
 
   # This function checks if the latest error log is created and tails it for docker logs uses.
-  if find "/opt/wireguarddashboard/app/src/log" -mindepth 1 -maxdepth 1 -type f | read -r; then
-    latestErrLog=$(find /opt/wireguarddashboard/app/src/log -name "error_*.log" | head -n 1)
-    latestAccLog=$(find /opt/wireguarddashboard/app/src/log -name "access_*.log" | head -n 1)
+  if find "/opt/wireguarddashboard/src/log" -mindepth 1 -maxdepth 1 -type f | read -r; then
+    latestErrLog=$(find /opt/wireguarddashboard/src/log -name "error_*.log" | head -n 1)
+    latestAccLog=$(find /opt/wireguarddashboard/src/log -name "access_*.log" | head -n 1)
     tail -f "${latestErrLog}" "${latestAccLog}"
   fi
 
