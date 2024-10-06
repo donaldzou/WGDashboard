56744cec7bec2d7259c3114c7fb4ba83500f900c
Donald Zou <donaldzou@live.hk>
2024-10-06 23:09:55 +0800

    Commit

--- src/static/app/build.sh
+++ src/static/app/build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-# Step 1: Run vite build
 echo "Running vite build..."
 if vite build; then
   echo "Vite build successful."
@@ -10,17 +8,16 @@ else
 fi
 echo "Checking for changes to commit..."
 if git diff-index --quiet HEAD --; then
-  echo "No changes to commit. Skipping commit."
-else
+  
   if git commit -a; then
-    echo "Git commit successful."
-  else
-    echo "Git commit failed. Exiting."
-    exit 1
-  fi
+      echo "Git commit successful."
+    else
+      echo "Git commit failed. Exiting."
+      exit 1
+    fi
+else
+  echo "No changes to commit. Skipping commit."
 fi
-
-# Step 3: Push changes to remote
 echo "Pushing changes to remote..."
 if git push; then
   echo "Git push successful."
