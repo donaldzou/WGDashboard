f089e4a1ce10d51ebb243fb25e6b63f0e6d9ef3f
Donald Zou <donaldzou@live.hk>
2024-11-07 00:09:40 +0800

    update

--- src/static/app/vite.config.js
+++ src/static/app/vite.config.js
@@ -3,68 +3,61 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import {proxy} from "./proxy.js";
 import vue from '@vitejs/plugin-vue'
-import {v4} from "uuid";
+import { version } from './package.json'
 
-const hash = () => {
-    return Math.floor(Math.random() * 90000) + 10000
-}
-
-// https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
-  
+	if (mode === 'electron'){
+		return {
+			emptyOutDir: false,
+			base: './',
+			plugins: [
+				vue(),
+			],
+			resolve: {
+				alias: {
+					'@': fileURLToPath(new URL('./src', import.meta.url))
+				}
+			},
+			build: {
+				target: "es2022",
+				outDir: '../../../../WGDashboard-Desktop',
+				rollupOptions: {
+					output: {
+						entryFileNames: `assets/[name].js?v=${version}`,
+						chunkFileNames: `assets/[name].js?v=${version}`,
+						assetFileNames: `assets/[name].[ext]?v=${version}`
+					}
+				}
+			}
+		}
+	}
 
-  if (mode === 'electron'){
-    return {
-      emptyOutDir: false,
-      base: './',
-      plugins: [
-        vue(),
-      ],
-      resolve: {
-        alias: {
-          '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-      },
-      build: {
-        target: "es2022",
-        outDir: '../../../../WGDashboard-Desktop',
-        rollupOptions: {
-          output: {
-            entryFileNames: `assets/[name]-${v4()}.js`,
-            chunkFileNames: `assets/[name]-${v4()}.js`,
-            assetFileNames: `assets/[name]-${v4()}.[ext]`
-          }
-        }
-      }
-    }
-  }
-
-  return {
-    base: "/static/app/dist",
-    plugins: [
-      vue(),
-    ],
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      }
-    },
-    server:{
-      proxy: {
-        '/api': proxy
-      },
-      host: '0.0.0.0'
-    },
-    build: {
-      target: "es2022",
-      outDir: 'dist',
-      rollupOptions: {
-        output: {
-          entryFileNames: `assets/[name]-${v4()}.js`,
-          chunkFileNames: `assets/[name]-${v4()}.js`,
-          assetFileNames: `assets/[name]-${v4()}.[ext]`
-        }
-      }
-    }
-  }
+	return {
+		base: "/static/app/dist",
+		plugins: [
+			vue(),
+		],
+		resolve: {
+			alias: {
+				'@': fileURLToPath(new URL('./src', import.meta.url))
+			}
+		},
+		server:{
+			proxy: {
+				'/api': proxy
+			},
+			host: '0.0.0.0'
+		},
+		build: {
+			target: "es2022",
+			outDir: 'dist',
+			rollupOptions: {
+				output: {
+					entryFileNames: `assets/[name].js?v=${version}`,
+					chunkFileNames: `assets/[name].js?v=${version}`,
+					assetFileNames: `assets/[name].[ext]?v=${version}`
+				}
+			}
+		}
+	}
 })
