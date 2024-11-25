56be95ff51cbeab4e838abb7233b07c9ad1ac93b
Donald Zou <donaldzou@live.hk>
2024-11-25 16:06:25 +0800

    Update dashboard.py

--- src/dashboard.py
+++ src/dashboard.py
@@ -89,25 +89,6 @@ class CustomJsonEncoder(DefaultJSONProvider):
 
 
 app.json = CustomJsonEncoder(app)
-
-# class PeerShareLink:
-#     def __init__(self, ShareID:str, Configuration: str, Peer: str, ExpireDate: datetime, ShareDate: datetime):
-#         self.ShareID = ShareID
-#         self.Peer = Peer
-#         self.Configuration = Configuration
-#         self.ShareDate = ShareDate
-#         self.ExpireDate = ExpireDate
-#         
-#     
-#     def toJson(self):
-#         return {
-#             "ShareID": self.ShareID,
-#             "Peer": self.Peer,
-#             "Configuration": self.Configuration,
-#             "ExpireDate": self.ExpireDate
-#         }
-
-
         
 class WireguardConfiguration:
     class InvalidConfigurationFileException(Exception):
