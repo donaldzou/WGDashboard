7c70fbec30ffffe45c8136311c9926fbb4de3fbe
Donald Zou <donaldzou@live.hk>
2025-02-14 23:24:16 +0800

    Build

--- src/static/app/dist/assets/localeText-GaOVxp1i.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,G as t,a as o,c as a,t as c}from"./index-Dtibxnfz.js";const s={name:"localeText",props:{t:""},computed:{getLocaleText(){return t(this.t)}}};function n(r,p,l,_,i,x){return o(),a("span",null,c(this.getLocaleText),1)}const u=e(s,[["render",n]]);export{u as L};
