df7f9f2b1400aaaedaea1b13c445fc0fe9090195
Donald Zou <donaldzou@live.hk>
2025-05-31 20:52:53 +0800

    Finished initializing client project

--- src/static/client/src/main.js
+++ src/static/client/src/main.js
@@ -1,14 +1,12 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
-import router from './router'
+import router from "@/router/router.js";
+import {createPinia} from "pinia";
 
-const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+createApp(App)
+	.use(createPinia())
+	.use(router)
+	.mount('#app')
--- src/static/client/vite.config.js
+++ src/static/client/vite.config.js
@@ -6,21 +6,14 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: "/static/client/dist",
 	plugins: [
 		vue(),
 		vueDevTools(),
-		{
-			name: 'rename',
-			enforce: 'post',
-			generateBundle(options, bundle) {
-				bundle['index.html'].fileName = bundle['index.html'].fileName.replace('index.html', 'client.html')
-			}
-		}
 	],
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url))
 		},
-	}
+	},
+	base: '/static/client/dist'
 })
