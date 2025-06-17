90e6409b1e0cfde08bcae7739e377b4761bf165c
Donald Zou <donaldzou@live.hk>
2025-06-17 13:34:13 +0800

    Update dashboard.py
    Update version number

--- src/dashboard.py
+++ src/dashboard.py
@@ -25,7 +25,7 @@ from modules.PeerJob import PeerJob
 from modules.SystemStatus import SystemStatus
 SystemStatus = SystemStatus()
 
-DASHBOARD_VERSION = 'v4.2.3'
+DASHBOARD_VERSION = 'v4.2.4'
 
 CONFIGURATION_PATH = os.getenv('CONFIGURATION_PATH', '.')
 DB_PATH = os.path.join(CONFIGURATION_PATH, 'db')
