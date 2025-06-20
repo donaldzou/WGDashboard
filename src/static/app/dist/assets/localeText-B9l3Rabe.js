68d8546383bdb691b83986dbc60392b47eefcbe6
Donald Zou <donaldzou@live.hk>
2025-06-20 16:01:20 +0800

    Build

---
+++ src/static/app/dist/assets/localeText-B9l3Rabe.js
@@ -0,0 +1,5 @@
+<<<<<<<< HEAD:src/static/app/dist/assets/localeText-CuybU_0U.js
+import{_ as e,G as t,a as o,c as a,t as c}from"./index-oBQzjt8-.js";const s={name:"localeText",props:{t:""},computed:{getLocaleText(){return t(this.t)}}};function n(r,p,l,_,i,x){return o(),a("span",null,c(this.getLocaleText),1)}const u=e(s,[["render",n]]);export{u as L};
+========
+import{_ as e,G as t,a as o,c as a,t as c}from"./index-DMmawzh_.js";const s={name:"localeText",props:{t:""},computed:{getLocaleText(){return t(this.t)}}};function n(r,p,l,_,i,x){return o(),a("span",null,c(this.getLocaleText),1)}const u=e(s,[["render",n]]);export{u as L};
+>>>>>>>> fc7bbf8 (Build):src/static/app/dist/assets/localeText-B9l3Rabe.js
