1a9f86e0381ba04dd452d77e436435b839ec612c
GwyNux <gwynux.web@gmail.com>
2025-06-25 13:57:53 +0300

    Delete patch_getremoteendpoint.py

--- patch_getremoteendpoint.py
+++
@@ -1,8 +0,0 @@
-# Simple stub for GetRemoteEndpoint
-
-def GetRemoteEndpoint():
-    return "127.0.0.1"
-
-if __name__ == "__main__":
-    # Тест: вывести результат в консоль
-    print(GetRemoteEndpoint())
