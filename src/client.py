f633a9654a538d96c9d050e4be8c59d733a5d49b
Donald Zou <donaldzou@live.hk>
2025-07-02 18:51:58 +0800

    Update

--- src/client.py
+++ src/client.py
@@ -28,11 +28,7 @@ def login_required(f):
 
 def createClientBlueprint(wireguardConfigurations: dict[WireguardConfiguration], dashboardConfig: DashboardConfig):
     from modules.DashboardClients import DashboardClients
-    
-    
-    
-    with current_app.app_context():
-        DashboardClients = DashboardClients(wireguardConfigurations)
+        
     client = Blueprint('client', __name__, template_folder=os.path.abspath("./static/client/dist"))
     prefix = f'{dashboardConfig.GetConfig("Server", "app_prefix")[1]}/client'
     
