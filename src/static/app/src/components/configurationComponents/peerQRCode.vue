c9f395acbd0150dd016f9298f63fff54c2a42f89
Donald Zou <donaldzou@live.hk>
2025-09-15 01:04:56 +0800

    Push

--- src/static/app/src/components/configurationComponents/peerQRCode.vue
+++ src/static/app/src/components/configurationComponents/peerQRCode.vue
@@ -69,10 +69,18 @@ export default {
 						<button type="button" class="btn-close ms-auto" @click="this.$emit('close')"></button>
 					</div>
 					<div class="card-body p-4">
-						<div  class="d-flex">
-							<canvas id="qrcode" class="rounded-3 shadow animate__animated animate__fadeIn animate__faster" 
-							        
-							        :class="{'d-none': loading}"></canvas>
+						<div class="d-flex gap-2">
+							<div class="d-flex flex-column gap-2 align-items-center">
+								<canvas id="qrcode" class="rounded-3 shadow animate__animated animate__fadeIn animate__faster"
+										:class="{'d-none': loading}"></canvas>
+								<LocaleText t="Scan with WireGuard App" class="text-muted" v-if='this.selectedPeer.configuration.Protocol === "wg"'></LocaleText>
+								<LocaleText t="Scan with AmneziaWG App" class="text-muted" v-if='this.selectedPeer.configuration.Protocol === "awg"'></LocaleText>
+							</div>
+							<div v-if='this.selectedPeer.configuration.Protocol === "awg"'>
+								<canvas id="awg_vpn_qrcode" class="rounded-3 shadow animate__animated animate__fadeIn animate__faster"
+										:class="{'d-none': loading}"></canvas>
+								<LocaleText t="Scan with AmneziaVPN App" class="text-muted"></LocaleText>
+							</div>
 							<div class="spinner-border m-auto" role="status" v-if="loading">
 								<span class="visually-hidden">Loading...</span>
 							</div>
