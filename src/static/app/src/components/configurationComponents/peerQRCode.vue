1d66cda2779473ea663b221ef7eb1e58e21d858f
Donald Zou <donaldzou@live.hk>
2025-09-15 11:39:16 +0800

    Push

--- src/static/app/src/components/configurationComponents/peerQRCode.vue
+++ src/static/app/src/components/configurationComponents/peerQRCode.vue
@@ -74,7 +74,9 @@ export default {
 					<div class="card-body p-4">
 						<div class="d-flex gap-2">
 							<div class="d-flex flex-column gap-2 align-items-center">
-								<canvas id="qrcode" class="rounded-3 shadow animate__animated animate__fadeIn animate__faster"
+								<canvas id="qrcode"
+										style="width: 200px !important; height: 200px !important;"
+										class="rounded-3 shadow animate__animated animate__fadeIn animate__faster"
 										:class="{'d-none': loading}"></canvas>
 								<LocaleText t="Scan with WireGuard App" class="text-muted" v-if='this.selectedPeer.configuration.Protocol === "wg"'></LocaleText>
 								<LocaleText t="Scan with AmneziaWG App" class="text-muted" v-if='this.selectedPeer.configuration.Protocol === "awg"'></LocaleText>
@@ -102,5 +104,10 @@ export default {
 		height: auto !important;
 		aspect-ratio: 1/1;
 	}
+}
+
+#qrcode{
+	width: 200px !important;
+	height: 200px !important;
 }
 </style>
