684a452013b7d33471fa1febc8454f8c950150d9
Daan Selen <dselen@nerthus.nl>
2025-09-19 18:48:13 +0200

    fix: logging somehow was broken due to disablement of external loggers

--- src/dashboard.py
+++ src/dashboard.py
@@ -215,7 +215,7 @@ app.logger.setLevel(getattr(logging, log_level.upper(), logging.INFO))
 
 dictConfig({
     'version': 1,
-    'disable_existing_loggers': True,
+    'disable_existing_loggers': False,
     'formatters': {
         'default': {
             'format': '[%(asctime)s] [%(levelname)s] in [%(module)s] %(message)s',
